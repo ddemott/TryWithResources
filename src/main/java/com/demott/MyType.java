78fff65dccacb846010f9db7618341dad6797341
ddemott <daledemott@gmail.com>
2018-05-16 04:46:19 +0100

    Updating .gitignore file

--- src/main/java/com/demott/MyType.java
+++ src/main/java/com/demott/MyType.java
@@ -1,16 +1,24 @@
 package com.demott;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+/**
+ * @author Dale DeMott
+ * 
+ *         This shows an example of how the try with resources works. With this
+ *         syntax you no longer need to close your resources when you are done
+ *         with it. As soon as you break scope, the resource is automatically
+ *         closed. This is excellent when you want to help reduce accidental
+ *         resource leaks.
+ */
+
 public class MyType implements Serializable {
 
 	private static final long serialVersionUID = -8737842418024012979L;
 
-	@Override
 	public String toString() {
 		return "This is a MyType";
 	}
@@ -24,14 +32,8 @@ public class MyType implements Serializable {
 			System.out.println("Done");
 			System.out.println(
 					"Once ran if you do a refresh on your project you'll see a file named myType.ser that was serialized and written out");
-		} catch (FileNotFoundException fnfe) {
-			fnfe.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		/** with this syntax you no longer need to close your resource **/
-		/** As soon as you break scope, the resource is automatically closed **/
-		/** This is excellent when you want to help reduce accidental **/
-		/** resource leaks **/
 	}
 }
